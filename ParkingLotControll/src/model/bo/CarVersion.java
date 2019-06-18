df8de3ae11c5bc852a27ed4c9f47034c81853275
pedro-ca <50923316+pedro-ca@users.noreply.github.com>
2019-06-18 11:41:58 -0300

    Add files via upload

--- ParkingLotControll/src/model/bo/CarVersion.java
+++ ParkingLotControll/src/model/bo/CarVersion.java
@@ -3,7 +3,6 @@ package model.bo;
 
 public class CarVersion {       //car version (e.g. sedan). One version can have many existing vehicles. 
     private String nameVersion;
-    private String typeCombus;
     private String typeFuel;
     private int numberSeats;
     private boolean hasBrakeAbs;
@@ -11,9 +10,8 @@ public class CarVersion {       //car version (e.g. sedan). One version can have
     private CarModel carModel;
     
 
-    public CarVersion(String nameVersion, String typeCombus, String typeFuel, int numberSeats, boolean hasBrakeAbs, boolean hasTransAuto, CarModel carModel) {
+    public CarVersion(String nameVersion, String typeFuel, int numberSeats, boolean hasBrakeAbs, boolean hasTransAuto, CarModel carModel) {
         this.nameVersion = nameVersion;
-        this.typeCombus = typeCombus;
         this.typeFuel = typeFuel;
         this.numberSeats = numberSeats;
         this.hasBrakeAbs = hasBrakeAbs;
@@ -32,14 +30,6 @@ public class CarVersion {       //car version (e.g. sedan). One version can have
 
     public void setNameVersion(String nameVersion) {
         this.nameVersion = nameVersion;
-    }
-
-    public String getTypeCombus() {
-        return typeCombus;
-    }
-
-    public void setTypeCombus(String typeCombus) {
-        this.typeCombus = typeCombus;
     }
 
     public String getTypeFuel() {
@@ -85,8 +75,7 @@ public class CarVersion {       //car version (e.g. sedan). One version can have
     
     @Override
     public String toString() {
-        return "CarVersion{" + "nameVersion=" + nameVersion + 
-                ", typeCombus=" + typeCombus + ", typeFuel=" + typeFuel + 
+        return "CarVersion{" + "nameVersion=" + nameVersion + ", typeFuel=" + typeFuel + 
                 ", numberSeats=" + numberSeats + ", hasBrakeAbs=" + hasBrakeAbs + 
                 ", hasTransAuto=" + hasTransAuto + ", carModel=" + carModel + '}';
     }   
